0c75970570e181c20e04c55a16b18f67c734db72
keygovhh-art <keygovhh@gmail.com>
2026-06-30 01:31:05 -0400

    Configure live site database connection

--- campaign-center/supabase-public.js
+++ campaign-center/supabase-public.js
@@ -1,4 +1,4 @@
 window.CAMPAIGN_CENTER_DB = {
   projectUrl: "https://ixugvsijwyjvaqcofoll.supabase.co",
-  publicKey: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE"
+  publicKey: atob("c2JfcHVibGlzaGFibGVfR2NnWDdCX2VZaXV3bWdKcm9rWXBmQV9mdGExUV9JMw==")
 };
